e41adb3375a97e685f24c133a961b875f7523e63
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2021-12-14 14:26:03 +0300

    Splash Screen

---
+++ app/src/main/java/com/example/mynba/IntroActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mynba
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.airbnb.lottie.LottieAnimationView
+
+class IntroActivity : AppCompatActivity() {
+
+    private lateinit var intro : LottieAnimationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+
+        intro = findViewById(R.id.animationView)
+
+        Handler().postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
